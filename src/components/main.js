87600e710c795d020b3473fe88fb8302d811d61b
antihero <mrcansler@gmail.com>
2020-04-27 13:06:50 -0500

    routes updated

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import LandingPage from './landingpage';
-import AboutMe from './aboutme';
+import Landing from './landingpage';
+import About from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
@@ -10,12 +10,12 @@ import Resume from './resume';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
+    <Route exact path="/" component={Landing} />
+    <Route path="/aboutme" component={About} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
   </Switch>
-)
+);
 
 export default Main;
